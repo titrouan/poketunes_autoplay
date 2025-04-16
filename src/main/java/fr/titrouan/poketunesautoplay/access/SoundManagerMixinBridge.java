3890a94eeb73ca90494178734886203b3f236517
titrouan <44969879+titrouan@users.noreply.github.com>
2025-04-16 22:55:53 +0200

    Delete src/main directory

--- src/main/java/fr/titrouan/poketunesautoplay/access/SoundManagerMixinBridge.java
+++
@@ -1,6 +0,0 @@
-package fr.titrouan.poketunesautoplay.access;
-
-public interface SoundManagerMixinBridge {
-    boolean poketunesautoplay$isMusicPlayingBridge();
-    void poketunesautoplay$resetMusicStatus();
-}
--- src/main/java/fr/titrouan/poketunesautoplay/access/VolumeAdjustable.java
+++
@@ -1,5 +0,0 @@
-package fr.titrouan.poketunesautoplay.access;
-
-public interface VolumeAdjustable {
-    void poketunesautoplay$setVolume(float volume);
-}
